bf846dd5ce4281dcce00d29ec9e4d8b15cd17273
botulina <botulina@hotmail.com>
2017-11-10 16:24:11 -0300

    Lesson 6
    Enron

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,40 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print enron_data['ALLEN PHILLIP K'].keys() 
+print len(enron_data['ALLEN PHILLIP K'].keys()), "# keys"
 
+
+poi = 0
+for i in enron_data:
+    if enron_data[i]["poi"]==1:
+        poi += 1
+print poi, "POI"
+
+enron_names = open("../final_project/poi_names.txt").read().split('\n')
+print len(enron_names), "NAMES"
+
+poi_y = [a for a in enron_names if "(y)" in a]
+print len(poi_y), "POI(Y)"
+
+poi_n = [a for a in enron_names if "(n)" in a]
+print len(poi_n), "POI(N)"
+
+print enron_data['PRENTICE JAMES']['total_stock_value'], "PRENTICE JAMES']['total_stock_value']"
+print enron_data['COLWELL WESLEY']['from_this_person_to_poi'], "['COLWELL WESLEY']['from_this_person_to_poi']"
+print enron_data['SKILLING JEFFREY K']['exercised_stock_options'], "['SKILLING JEFFREY K']['exercised_stock_options']"
+print enron_data['SKILLING JEFFREY K']['total_payments'], "['SKILLING JEFFREY K']['total_payments']"
+print enron_data['FASTOW ANDREW S']['total_payments'], "['FASTOW ANDREW S']['total_payments']"
+print enron_data['LAY KENNETH L']['total_payments'], "['LAY KENNETH L']['total_payments']"
+
+email = 0
+for i in enron_data:
+    if enron_data[i]['email_address'] != 'NaN':
+        email += 1
+print 'email', email
+
+salary = 0
+for i in enron_data:
+    if enron_data[i]['salary'] != 'NaN':
+        salary += 1
+print 'salary', salary
