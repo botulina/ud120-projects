693dc24acd5f4bb865f56f4719fd2296efd010e5
botulina <botulina@hotmail.com>
2017-11-29 11:32:53 -0300

    Lesson 16

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -28,4 +28,37 @@ labels, features = targetFeatureSplit(data)
 
 ### your code goes here 
 
+from sklearn.cross_validation import train_test_split
+from sklearn import tree
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.30, random_state=42)
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+acc = round(accuracy_score(labels_test, pred), 5)
+print "Accuracy:", acc
+poi = 0
+for ii in labels_test:
+    if ii == 1:
+        poi += 1
+print "POIs=", poi
+tpos = 0
+for n in range(len(pred)):
+    if pred[n] == 1 and labels_test[n] == 1:
+        tpos += 1
+print "True Positive:", tpos
+print "Presicion:", precision_score(labels_test, pred)
+print "Recall:", recall_score(labels_test, pred)
+print f1_score(labels_test, pred)
+
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1] 
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+
+for n in range(len(predictions)):
+    if predictions[n] == 0 and true_labels[n] == 0:
+        tpos += 1
+print "True Positive TEST:", tpos
+print "Presicion TEST:", precision_score(true_labels, predictions)
+print "Recall TEST:", recall_score(true_labels, predictions)
+print f1_score(true_labels, predictions)
